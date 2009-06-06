d488387964317f5df559f2765a6965867ddd5d28
Skylar Woodward <skylar@craver.kiva.local>
2009-06-06 15:02:20 -0700

    Adding in the rest of the SproutCore template project

---
+++ clients/klb/models/loan.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// Klb.Loan
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document your class here)
+
+  @extends SC.Record
+  @author AuthorName
+  @version 0.1
+*/
+Klb.Loan = SC.Record.extend(
+/** @scope Klb.Loan.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
