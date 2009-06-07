716f1092cb2ab4327931c26f28d2ad2904317957
King Chung Huang <king@king-mbp.local>
2009-06-06 21:27:09 -0700

    Created basic kiva loans data source.

---
+++ apps/klb/controllers/kiva_data_source.js
@@ -0,0 +1,100 @@
+// ==========================================================================
+// Project:   Klb.kivaDataSource
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Klb */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+
+Klb.KivaDataSource = SC.DataSource.extend(
+/** @scope Klb.KivaDataSource.prototype */ {
+
+	fetchRequest: SC.Request.getUrl("http://127.0.0.1:4020/v1/loans/newest.json").set('isJSON', YES),
+
+	fetch: function(store, fetchKey, params) {
+		var ret = [], url;
+		var action = {};
+		
+		console.log("About to fetch.");
+		
+		this.fetchRequest.notify(this, this.fetchDidComplete, {
+			store: store,
+			fetchKey: fetchKey,
+			storeKeyArray: ret
+		}).send();
+		
+		return ret;
+	},
+	
+	fetchDidComplete: function(r, params) {
+		console.log("Fetch did complete.");
+	
+		var storeKeys = [], store, fetchKey;
+		var response = r.response();
+		
+		// { loans: Array, paging: {} }
+		
+		loans = response['loans'];
+		for (var i = 0; i < loans.length; i++) {
+			loan = loans[i];
+			loan['guid'] = loan['id'];
+		}
+		
+		if (response.kindOf ? response.kindOf(SC.Error) : false) {		
+			this.requestDidError(r);
+		} else {
+			fetchKey = params.fetchKey;
+			storeKeys = params.store.loadRecords(fetchKey, loans);
+			params.storeKeyArray.replace(0, 0, storeKeys);
+		}
+		
+		return YES;
+	},
+	
+	retrieveRequest: SC.Request.getUrl("http://127.0.0.1/v1/").set('isJSON', YES),
+	
+	retrieveRecord: function(store, storeKey) {
+		var ret = [], url, action = {},
+				recordType = SC.Store.recordTypeFor(storeKey),
+				id = recordType.idFor(storeKey);
+		url = 'loans/' + id + '.json';
+		
+		this.retrieveRequest.set('address', url);
+		this.retrieveRequest.notify(this, this.retrieveRecordDidComplete, {
+			store: store,
+			storeKey: storeKey,
+			id: id
+		}).send();
+		
+		return ret;
+	},
+	
+	retrieveRecordDidComplete: function(r, params) {
+		var response, results, dataHash, storeKeys = [], hashes = [];
+		response = r.response();
+		
+		if (response.kindOf ? response.kindOf(SC.Error) : false) {
+			this.requestDidError(r);
+		} else {
+			results = response.content;
+			dataHash = results;
+			hashes.push(dataHash);
+			storeKeys.push(params.storeKey);
+			params.store.dataSourceDidComplete(params.storeKey, dataHash, params.id);
+		}
+		
+		return YES;
+	},
+	
+	requestDidError: function(r) {
+		var pane = SC.AlertPane.error("There has been an error with your request", r.get('rawResponse').toString(), '', "OK", "Cancel", this);
+	
+		return YES;
+	},
+
+});
---
+++ apps/klb/tests/controllers/kiva_data_source.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Klb.kivaDataSource Unit Test
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Klb module test ok equals same stop start */
+
+module("Klb.kivaDataSource");
+
+// TODO: Replace with real unit test for Klb.kivaDataSource
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
