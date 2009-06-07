eaf959c07e387b686158a597cd50110aff6abcd6
Skylar Woodward <skylar@craver.kiva.local>
2009-06-06 21:32:09 -0700

    adding some more pretty widgets to the view

--- apps/klb/english.lproj/main_page.js
+++ apps/klb/english.lproj/main_page.js
@@ -10,30 +10,49 @@ Klb.mainPage = SC.Page.design({
   // The main pane is made visible on screen as soon as your app is loaded.
   // Add childViews to this pane for views to display immediately on page 
   // load.
-  mainPane: SC.MainPane.design({
-	  childViews: 'topView middleView bottomView'.w(),
+  appPane: SC.MainPane.design({
+	  childViews: 'sidebarView mainView'.w(),
 
-	  topView: SC.View.design(SC.Border, {
-	    layout: { top: 0, left: 0, right: 0, height: 41 },
-	    borderStyle: SC.BORDER_BOTTOM
+	  sidebarView: SC.View.design(SC.Border, {
+	    layout: { top: -1, left: -1, right: 0, width: 200 },
+	    borderStyle: SC.BORDER_GRAY
 	  }),
 
-	  middleView: SC.ScrollView.design({
-	    hasHorizontalScroller: NO,
-	    layout: { top: 42, bottom: 42, left: 0, right: 0 },
-	    backgroundColor: 'white',
+	  mainView: SC.View.design({
+			layout: { top: 0, left: 201, right: 0 },
+			childViews: 'topView loanView'.w(),
 
-			contentView: SC.ListView.design({
-			  contentBinding: 'Klb.loansController.arrangedObjects',
-			  selectionBinding: 'Klb.loansController.selection',
-				contentValueKey: "name"
-			})
-	  }),
+		  topView: SC.View.design(SC.Border, {
+		    layout: { top: 0, left: 0, right: 0, height: 60 },
+		    borderStyle: SC.BORDER_BOTTOM,
+				childViews: 'totalMatchesView sortListView'.w(),
+				
+				totalMatchesView: SC.LabelView.design({
+				      layout: { centerY: 0, height: 24, left: 8, width: 300 },
+				      controlSize: SC.LARGE_CONTROL_SIZE,
+				      fontWeight: SC.BOLD_WEIGHT,
+				      value:   '234 Loans Found'
+				    }),
+				
+				sortListView: SC.LabelView.design({
+				      layout: { centerY: 0, height: 24, right: 8, width: 200 },
+	            textAlign: SC.ALIGN_RIGHT,
+				      value:   'Sort results by Popularity'
+				    }),
+		  }),
 
-	  bottomView: SC.View.design(SC.Border, {
-	    layout: { bottom: 0, left: 0, right: 0, height: 41 },
-	    borderStyle: SC.BORDER_TOP
-	  })
+		  loanView: SC.ScrollView.design({
+		    hasHorizontalScroller: NO,
+		    layout: { top: 61, bottom: 0, left: 0, right: 0 },
+		    backgroundColor: 'white',
+
+				contentView: SC.ListView.design({
+				  contentBinding: 'Klb.loansController.arrangedObjects',
+				  selectionBinding: 'Klb.loansController.selection',
+					contentValueKey: "name"
+				})
+		  })
+		})
+
 	})
-
 });
--- apps/klb/main.js
+++ apps/klb/main.js
@@ -17,7 +17,7 @@ Klb.main = function main() {
   // The default code here will make the mainPane for your application visible
   // on screen.  If you app gets any level of complexity, you will probably 
   // create multiple pages and panes.  
-  Klb.getPath('mainPage.mainPane').append();
+  Klb.getPath('mainPage.appPane').append();
 
   // Step 2. Set the content property on your primary controller.
   // This will make your app come alive!
