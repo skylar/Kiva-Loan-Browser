259965a2c455a6b3656436a238f477bfdbc372ce
Charles Jolley <charles@sproutit.com>
2009-08-11 11:28:38 -0700

    Add initial legal page

---
+++ pages/legal/core.js
@@ -0,0 +1,22 @@
+// ==========================================================================
+// Project:   Legal
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Legal */
+
+/** @namespace
+
+  Static page JavaScript
+  
+  @extends SC.Object
+*/
+Legal = SC.Object.create(
+  /** @scope Legal.prototype */ {
+
+  NAMESPACE: 'Legal',
+  VERSION: '0.1.0'
+  
+  // TODO: You can place any JavaScript you need on your page here.  If you
+  // don't want any JavaScript, just delete this file.
+
+}) ;
